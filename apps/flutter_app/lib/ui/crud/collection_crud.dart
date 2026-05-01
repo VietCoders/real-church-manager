d33becef02f53bb3ca8536c3959066d2170ee37b
VietCoders <89422548+VietCoders@users.noreply.github.com>
2026-05-01 09:34:33 +0700

    snapshot: before edit

--- apps/flutter_app/lib/ui/crud/collection_crud.dart
+++ apps/flutter_app/lib/ui/crud/collection_crud.dart
@@ -4,6 +4,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 import 'package:intl/intl.dart';
 import 'package:pocketbase/pocketbase.dart';
 
